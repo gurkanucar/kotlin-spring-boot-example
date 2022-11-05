95fd8a31a0991b341032f1477071b9fb28777b81
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-05 15:51:37 +0300

    converter extensions updated

---
+++ src/main/kotlin/com/example/kotlinspringbootexample/converter/AddressConverter.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinspringbootexample.converter
+
+import com.example.kotlinspringbootexample.dto.AddressDTO
+import com.example.kotlinspringbootexample.model.Address
+import com.example.kotlinspringbootexample.model.User
+import com.example.kotlinspringbootexample.request.CreateAddressRequest
+import com.example.kotlinspringbootexample.request.CreateUserRequest
+import com.example.kotlinspringbootexample.request.UpdateAddressRequest
+
+fun Address.toDTO(): AddressDTO {
+            return AddressDTO(
+                id = this.id,
+                title = this.title,
+                country = this.country,
+                city = this.city,
+                town = this.town,
+                detail = this.detail
+            )
+        }
+
+        fun CreateAddressRequest.toAddress(user: User): Address {
+            return Address(
+                user = user,
+                title = this.title,
+                country = this.country,
+                city = this.city,
+                town = this.town,
+                detail = this.detail
+            )
+        }
+
+        fun UpdateAddressRequest.toAddress(existing: Address): Address {
+            return Address(
+                id = existing.id,
+                user = existing.user,
+                title = this.title,
+                country = this.country,
+                city = this.city,
+                town = this.town,
+                detail = this.detail
+            )
+        }
---
+++ src/main/kotlin/com/example/kotlinspringbootexample/converter/UserConverter.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinspringbootexample.converter
+
+import com.example.kotlinspringbootexample.dto.UserDTO
+import com.example.kotlinspringbootexample.model.User
+import com.example.kotlinspringbootexample.request.CreateUserRequest
+
+        fun User.toDTO(): UserDTO {
+            return UserDTO(
+                id = this.id,
+                username = this.username,
+                email = this.email,
+                name = this.name,
+                surname = this.surname,
+            )
+        }
+
+
+        fun CreateUserRequest.toUser(): User {
+            return User(
+                username = this.username,
+                email = this.email,
+                name = this.name,
+                surname = this.surname
+            )
+        }
