43ca808d7df8680cd035aef69321493681500dfd
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-05 16:04:57 +0300

    code formatted

--- src/main/kotlin/com/example/kotlinspringbootexample/converter/AddressConverter.kt
+++ src/main/kotlin/com/example/kotlinspringbootexample/converter/AddressConverter.kt
@@ -8,35 +8,35 @@ import com.example.kotlinspringbootexample.request.CreateUserRequest
 import com.example.kotlinspringbootexample.request.UpdateAddressRequest
 
 fun Address.toDTO(): AddressDTO {
-            return AddressDTO(
-                id = this.id,
-                title = this.title,
-                country = this.country,
-                city = this.city,
-                town = this.town,
-                detail = this.detail
-            )
-        }
+    return AddressDTO(
+        id = this.id,
+        title = this.title,
+        country = this.country,
+        city = this.city,
+        town = this.town,
+        detail = this.detail
+    )
+}
 
-        fun CreateAddressRequest.toAddress(user: User): Address {
-            return Address(
-                user = user,
-                title = this.title,
-                country = this.country,
-                city = this.city,
-                town = this.town,
-                detail = this.detail
-            )
-        }
+fun CreateAddressRequest.toAddress(user: User): Address {
+    return Address(
+        user = user,
+        title = this.title,
+        country = this.country,
+        city = this.city,
+        town = this.town,
+        detail = this.detail
+    )
+}
 
-        fun UpdateAddressRequest.toAddress(existing: Address): Address {
-            return Address(
-                id = existing.id,
-                user = existing.user,
-                title = this.title,
-                country = this.country,
-                city = this.city,
-                town = this.town,
-                detail = this.detail
-            )
-        }
+fun UpdateAddressRequest.toAddress(existing: Address): Address {
+    return Address(
+        id = existing.id,
+        user = existing.user,
+        title = this.title,
+        country = this.country,
+        city = this.city,
+        town = this.town,
+        detail = this.detail
+    )
+}
--- src/main/kotlin/com/example/kotlinspringbootexample/converter/UserConverter.kt
+++ src/main/kotlin/com/example/kotlinspringbootexample/converter/UserConverter.kt
@@ -4,22 +4,22 @@ import com.example.kotlinspringbootexample.dto.UserDTO
 import com.example.kotlinspringbootexample.model.User
 import com.example.kotlinspringbootexample.request.CreateUserRequest
 
-        fun User.toDTO(): UserDTO {
-            return UserDTO(
-                id = this.id,
-                username = this.username,
-                email = this.email,
-                name = this.name,
-                surname = this.surname,
-            )
-        }
+fun User.toDTO(): UserDTO {
+    return UserDTO(
+        id = this.id,
+        username = this.username,
+        email = this.email,
+        name = this.name,
+        surname = this.surname,
+    )
+}
 
 
-        fun CreateUserRequest.toUser(): User {
-            return User(
-                username = this.username,
-                email = this.email,
-                name = this.name,
-                surname = this.surname
-            )
-        }
+fun CreateUserRequest.toUser(): User {
+    return User(
+        username = this.username,
+        email = this.email,
+        name = this.name,
+        surname = this.surname
+    )
+}
